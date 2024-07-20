3ec873ff3c0c68caf2c85eed000c0646d98ce8e7
giftedpromise <giftedpromise1992@gmail.com>
2024-07-20 11:20:04 -1200

    Dashboard overview

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MainLayout from './components/Layout/MainLayout';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    
     <Router>
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/dashboard"
+            element={
+              <div
+                className="flex-col h-screen w-[100%] overflow-auto"
+                style={{
+                  backgroundColor:
+                    'var(--Color-Style-Primary-Roles-Pri-container, #E2DDFF)',
+                }}
+              >
+                <Dashboard />
+              </div>
+            }
+          />
         </Routes>
       </MainLayout>
     </Router>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-bg': '#C0BDFF',
+      },
+      colors: {
+        'primary-50': 'var(--Color-Style-Primary-Roles-Pri-container, #E2DDFF)',
+      },
+      height: {
+        1092: '1092px',
+      },
+    },
   },
   plugins: [],
-}
+};
